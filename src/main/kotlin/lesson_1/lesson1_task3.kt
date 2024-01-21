f1c7beecdee79cf9ac68dc0a737ab021c44b2127
David <ffff0228@bk.ru>
2024-01-21 19:55:54 +0300

    KS-1-3 done with string data

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,16 +1,20 @@
 package org.example.lesson_1
 
 fun main() {
-    val year = 1961
-    var hour = 9
-    var minute = 7
+    val year = "1961"
+    val departureTime = "09:07"
+    val arrivalTime = "10:55"
+    val partsOfDepartureTime = departureTime.split(':')
+    val partsOfArrivalTime = arrivalTime.split(':')
+    var hour = partsOfDepartureTime[0]
+    var minute = partsOfDepartureTime[1]
 
     println("Год полета: $year")
     println("Час взлета: $hour")
     println("Минута взлета: $minute")
 
-    hour = 10
-    minute = 55
+    hour = partsOfArrivalTime[0]
+    minute = partsOfArrivalTime[1]
 
     print("Время посадки: $hour:$minute")
 }
