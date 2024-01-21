f28e03251a8099481020d987cf4de3487fc312a3
David <ffff0228@bk.ru>
2024-01-21 09:51:52 +0300

    KS-2-1 average mark

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_2
+
+fun main() {
+    val mark1 = 3
+    val mark2 = 4
+    val mark3 = 3
+    val mark4 = 5
+    val numberOfStudents = 4.0f
+
+    val average = (mark1 + mark2 + mark3 + mark4) / numberOfStudents
+    println(average)
+}
