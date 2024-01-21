7e153083306bdb3cf34266ef03528661e2eadb18
David <ffff0228@bk.ru>
2024-01-21 07:38:31 +0300

    KS-1-3 some information about Gagarin

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_1
+
+fun main() {
+    val year = 1961
+    var hour = 9
+    var minute = 7
+
+    println("Год полета: $year")
+    println("Час взлета: $hour")
+    println("Минута взлета: $minute")
+
+    hour = 10
+    minute = 55
+
+    print("Время посадки: $hour:$minute")
+}
