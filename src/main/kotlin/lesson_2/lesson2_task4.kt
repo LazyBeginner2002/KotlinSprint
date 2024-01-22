78f9ba7401d51fdc5a336ddadce4936ac886c05b
David <ffff0228@bk.ru>
2024-01-22 15:25:34 +0300

    KS-2-4 condition corrected

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,10 +1,13 @@
 package org.example.lesson_2
 
+const val PERCENT_TO_REAL_NUMBER = 100
+
 fun main() {
     val crystalOre = 7
     val ironOre = 11
-    val bonusMultiplier = 0.2f
+    val bonusPercent = 20.0f
 
+    val bonusMultiplier = bonusPercent/PERCENT_TO_REAL_NUMBER
     val bonusCrystalOre = (crystalOre * bonusMultiplier).toInt()
     val bonusIronOre = (ironOre * bonusMultiplier).toInt()
 
