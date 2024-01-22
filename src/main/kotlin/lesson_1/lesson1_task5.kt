d82b3c6e00516fde00684a6d06e8a56b40ea4129
David <ffff0228@bk.ru>
2024-01-22 15:01:19 +0300

    KS-1-5 consts created

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,10 +1,14 @@
 package org.example.lesson_1
 
+const val SEC_IN_HOUR = 3600
+const val SEC_IN_MIN = 60
+
 fun main() {
     val seconds = 6480
-    val hour = seconds / 3600
-    val minute = (seconds - (hour * 3600)) / 60
-    val second = seconds - (hour * 3600) - (minute * 60)
+
+    val hour = seconds / SEC_IN_HOUR
+    val minute = (seconds - hour * SEC_IN_HOUR) / SEC_IN_MIN
+    val second = seconds - hour * SEC_IN_HOUR - (minute * SEC_IN_MIN)
 
     println(String.format("Время, которое Гагарин провел в космосе: %02d:%02d:%02d", hour, minute, second))
 }
