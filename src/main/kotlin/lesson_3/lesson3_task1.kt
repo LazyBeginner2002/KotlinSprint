c4214fa6ad46d48b11b59894ee7f1e84eb4515c4
David <ffff0228@bk.ru>
2024-01-23 16:37:07 +0300

    KS-3-1 removed unnecessary symbols from the variables

--- src/main/kotlin/lesson_3/lesson3_task1.kt
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -2,9 +2,9 @@ package org.example.lesson_3
 
 fun main() {
     val name = "David"
-    var greeting = "Good afternoon,  "
-    println("$greeting$name!")
+    var greeting = "Good afternoon"
+    println("$greeting, $name!")
 
-    greeting = "Good evening, "
-    println("$greeting$name!")
+    greeting = "Good evening"
+    println("$greeting, $name!")
 }
