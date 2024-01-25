321e718e3141c38a119319696ed379df6d363a5b
David <ffff0228@bk.ru>
2024-01-25 21:04:16 +0300

    KS-4-3 added consts

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,12 +1,18 @@
 package org.example.lesson_4
 
+const val SUNNY_WEATHER = true
+const val OPENED_AWNING = true
+const val AIR_HUMIDITY = 20
+const val UNFAVORABLE_SEASON = "winter"
+
 fun main() {
     val weatherSunny = true
     val awningOpened = true
-    val season = "winter"
+    val currentSeason = "winter"
     val airHumidity = 20
 
-    val favorableConditions = weatherSunny && awningOpened && (season != "winter") && (airHumidity == 20)
+    val favorableConditions = (weatherSunny == SUNNY_WEATHER) && (awningOpened == OPENED_AWNING) &&
+            (currentSeason != UNFAVORABLE_SEASON) && (airHumidity == AIR_HUMIDITY)
     println("Благоприятные ли условия сейчас для роста бобовых? $favorableConditions")
 }
 
