624924c01abc5b0a88c0b0a14bd48529ad5ce22b
David <ffff0228@bk.ru>
2024-01-25 23:50:04 +0300

    KS-4-2 deleted 2 variables

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -5,16 +5,17 @@ const val MAX_WEIGHT_FOR_AVERAGE = 100
 const val MAX_VOLUME_FOR_AVERAGE = 100
 
 fun main() {
-    val weight1 = 20.0f
-    val volume1 = 80.0f
-    val weight2 = 50.0f
-    val volume2 = 100.0f
+    var weight = 20.0f
+    var volume = 80.0f
 
-    println("Груз с весом $weight1 и объемом $volume1 соответствует категории 'Average': " +
-            "${(weight1 >= MIN_WEIGHT_FOR_AVERAGE) && (weight1 <= MAX_WEIGHT_FOR_AVERAGE) 
-                    && volume1 <= MAX_VOLUME_FOR_AVERAGE}")
+    println("Груз с весом $weight и объемом $volume соответствует категории 'Average': " +
+            "${(weight >= MIN_WEIGHT_FOR_AVERAGE) && (weight <= MAX_WEIGHT_FOR_AVERAGE) 
+                    && volume < MAX_VOLUME_FOR_AVERAGE}")
 
-    println("Груз с весом $weight2 и объемом $volume2 соответствует категории 'Average': " +
-            "${(weight2 >= MIN_WEIGHT_FOR_AVERAGE) && (weight2 <= MAX_WEIGHT_FOR_AVERAGE)
-                    && volume2 <= MAX_VOLUME_FOR_AVERAGE}")
+    weight = 50.0f
+    volume = 100.0f
+
+    println("Груз с весом $weight и объемом $volume соответствует категории 'Average': " +
+            "${(weight >= MIN_WEIGHT_FOR_AVERAGE) && (weight <= MAX_WEIGHT_FOR_AVERAGE)
+                    && volume < MAX_VOLUME_FOR_AVERAGE}")
 }
