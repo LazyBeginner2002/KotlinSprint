0960583dbc6af262b043b2822a60affdca46c5f3
David <ffff0228@bk.ru>
2024-01-25 23:39:04 +0300

    KS-4-5 added consts

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -1,13 +1,21 @@
 package org.example.lesson_4
 
+const val MIN_ACCEPTABLE_NUMBER_OF_CREW = 55
+const val RECOMMENDED_NUMBER_OF_CREW = 70
+const val MIN_AMOUNT_OF_PROVISION = 51
+
 fun main() {
+    println("Введите входные данные: ")
     val presenceOfDamage = readln().toBoolean()
     val numberOfCrew = readln().toInt()
     val amountOfProvisions = readln().toInt()
     val favorableWeather = readln().toBoolean()
 
-    val allowedToSail = (!presenceOfDamage && (numberOfCrew in 55..70) && (amountOfProvisions > 50)) ||
-            ((numberOfCrew == 70) && favorableWeather && (amountOfProvisions > 50))
+    val allowedToSail = (!presenceOfDamage &&
+            (numberOfCrew in MIN_ACCEPTABLE_NUMBER_OF_CREW..RECOMMENDED_NUMBER_OF_CREW) &&
+            (amountOfProvisions >= MIN_AMOUNT_OF_PROVISION)) ||
+            ((numberOfCrew == RECOMMENDED_NUMBER_OF_CREW) && favorableWeather &&
+                    (amountOfProvisions >= MIN_AMOUNT_OF_PROVISION))
 
     println("Корабль может отправиться в плавание - $allowedToSail")
 }
