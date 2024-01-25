eb10cff4f99c9435b679ae44a51c9c0b8965581a
David <ffff0228@bk.ru>
2024-01-25 20:47:01 +0300

    KS-4-2 removed class

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -5,21 +5,16 @@ const val MAX_WEIGHT_FOR_AVERAGE = 100
 const val MAX_VOLUME_FOR_AVERAGE = 100
 
 fun main() {
-    val goods1 = Goods(20.0f,80.0f)
-    val goods2 = Goods(50.0f,100.0f)
+    val weight1 = 20.0f
+    val volume1 = 80.0f
+    val weight2 = 50.0f
+    val volume2 = 100.0f
 
-    println("Груз с весом ${goods1.weight} и объемом ${goods1.volume} соответствует категории 'Average': " +
-            "${(goods1.weight >= MIN_WEIGHT_FOR_AVERAGE) && (goods1.weight <= MAX_WEIGHT_FOR_AVERAGE) 
-                    && goods1.volume <= MAX_VOLUME_FOR_AVERAGE}")
+    println("Груз с весом $weight1 и объемом $volume1 соответствует категории 'Average': " +
+            "${(weight1 >= MIN_WEIGHT_FOR_AVERAGE) && (weight1 <= MAX_WEIGHT_FOR_AVERAGE) 
+                    && volume1 <= MAX_VOLUME_FOR_AVERAGE}")
 
-    println("Груз с весом ${goods2.weight} и объемом ${goods2.volume} соответствует категории 'Average': " +
-            "${(goods2.weight >= MIN_WEIGHT_FOR_AVERAGE) && (goods2.weight <= MAX_WEIGHT_FOR_AVERAGE)
-                    && goods2.volume <= MAX_VOLUME_FOR_AVERAGE}")
+    println("Груз с весом $weight2 и объемом $volume2 соответствует категории 'Average': " +
+            "${(weight2 >= MIN_WEIGHT_FOR_AVERAGE) && (weight2 <= MAX_WEIGHT_FOR_AVERAGE)
+                    && volume2 <= MAX_VOLUME_FOR_AVERAGE}")
 }
-
-class Goods (val weight: Float, val volume: Float)
-
-// Сделал с классом, ибо два объекта с одинаковыми св-вами.
-// Класс не засунул в отдельный файл, просто чтобы одним файлом была задача.
-// Вывод громоздкий. Я сделал бы функцию проверки на Average в классе, но в задании сказано сделать сравнение в print()
-
