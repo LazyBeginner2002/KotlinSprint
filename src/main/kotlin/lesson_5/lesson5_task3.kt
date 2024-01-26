5ab9962016c0f5dc89d0eb44c73268a1f9e1dddc
David <ffff0228@bk.ru>
2024-01-26 09:30:17 +0300

    KS-5-3 created if-else statements

---
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -0,0 +1,22 @@
+package org.example.lesson_5
+
+fun main() {
+    val firstWinNumber: UByte = 5u
+    val secondWinNumber: UByte = 13u
+
+    println("Введите ваши два числа:")
+    val firstNumber = readln().toUByte()
+    val secondNumber = readln().toUByte()
+
+    if ((firstNumber == firstWinNumber) or (firstNumber == secondWinNumber)) {
+        if ((secondNumber == firstWinNumber) or (secondNumber == secondWinNumber)) {
+            println("Поздравляем! Вы выиграли главный приз!")
+        } else {
+            println("Вы выиграли утешительный приз!")
+        }
+    } else if((secondNumber == firstWinNumber) or (secondNumber == secondWinNumber)){
+        println("Вы выиграли утешительный приз!")
+    } else {
+        println("Неудача!")
+    }
+}
