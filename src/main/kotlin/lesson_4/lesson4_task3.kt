b444a43195a1a08fd0e10d80cf66818c6c88af0e
David <ffff0228@bk.ru>
2024-01-27 11:15:29 +0300

    KS-4-3 fixes flag's names

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -6,12 +6,12 @@ const val AIR_HUMIDITY = 20
 const val UNFAVORABLE_SEASON = "winter"
 
 fun main() {
-    val weatherSunny = true
-    val awningOpened = true
+    val isWeatherSunny = true
+    val isAwningOpen = true
     val currentSeason = "winter"
     val airHumidity = 20
 
-    val favorableConditions = (weatherSunny == SUNNY_WEATHER) && (awningOpened == OPENED_AWNING) &&
+    val favorableConditions = (isWeatherSunny == SUNNY_WEATHER) && (isAwningOpen == OPENED_AWNING) &&
             (currentSeason != UNFAVORABLE_SEASON) && (airHumidity == AIR_HUMIDITY)
     println("Благоприятные ли условия сейчас для роста бобовых? $favorableConditions")
 }
