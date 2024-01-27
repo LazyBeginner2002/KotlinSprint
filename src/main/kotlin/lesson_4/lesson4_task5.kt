1161d816c61cd9436c31d605b8a0afe936229ffc
David <ffff0228@bk.ru>
2024-01-27 11:24:38 +0300

    KS-5-1 fixed ui

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -5,16 +5,20 @@ const val RECOMMENDED_NUMBER_OF_CREW = 70
 const val MIN_AMOUNT_OF_PROVISION = 51
 
 fun main() {
-    println("Введите входные данные: ")
-    val presenceOfDamage = readln().toBoolean()
+    println("Введите данные:")
+    print("- Наличие повреждений: ")
+    val isShipDamaged = readln().toBoolean()
+    print("- Количество экипажа: ")
     val numberOfCrew = readln().toInt()
+    print("- Количество провизии(шт. ящиков): ")
     val amountOfProvisions = readln().toInt()
-    val favorableWeather = readln().toBoolean()
+    print("- Благоприятная погода: ")
+    val isWeatherFavorable = readln().toBoolean()
 
-    val allowedToSail = (!presenceOfDamage &&
+    val allowedToSail = (!isShipDamaged &&
             (numberOfCrew in MIN_ACCEPTABLE_NUMBER_OF_CREW..RECOMMENDED_NUMBER_OF_CREW) &&
             (amountOfProvisions >= MIN_AMOUNT_OF_PROVISION)) ||
-            ((numberOfCrew == RECOMMENDED_NUMBER_OF_CREW) && favorableWeather &&
+            ((numberOfCrew == RECOMMENDED_NUMBER_OF_CREW) && isWeatherFavorable &&
                     (amountOfProvisions >= MIN_AMOUNT_OF_PROVISION))
 
     println("Корабль может отправиться в плавание - $allowedToSail")
