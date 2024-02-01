a32ea3754ee3a517e9a9eba334db7a6e1c79d4be
David <ffff0228@bk.ru>
2024-02-01 07:19:16 +0300

    KS-6-1 registration and authorization system

---
+++ src/main/kotlin/lesson_6/lesson6_task1.kt
@@ -0,0 +1,29 @@
+package org.example.lesson_6
+
+fun main() {
+    var readLogin: String
+    var readPassword: String
+
+    println("Здравствуйте! Для начала работы пройдите регистрацию.\nВведите логин: ")
+    val login = readln()
+    println("Введите пароль: ")
+    val password = readln()
+    println("Повторите пароль: ")
+    var repeatPassword = readln()
+
+    while (repeatPassword != password) {
+        println("Неверный пароль. Попробуйте еще раз.")
+        repeatPassword = readln()
+    }
+
+    println("Регистрация прошла успешно! Войдите в аккаунт.")
+
+    do {
+        print("Введите логин: ")
+        readLogin = readln()
+        print("Введите пароль: ")
+        readPassword = readln()
+    } while ((readLogin != login) || (readPassword != password))
+
+    println("Авторизация прошла успешно!")
+}
