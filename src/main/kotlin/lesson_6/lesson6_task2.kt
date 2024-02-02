7c04e6f117fce454890fbf7f8481c11ab7d5ed66
David <ffff0228@bk.ru>
2024-02-02 10:18:23 +0300

    KS-6-2 removed redundant while-cycle

--- src/main/kotlin/lesson_6/lesson6_task2.kt
+++ src/main/kotlin/lesson_6/lesson6_task2.kt
@@ -4,11 +4,8 @@ fun main() {
     print("Введите количество секунд, которые хотите засечь: ")
     val seconds = readln().toInt()
 
-    var counter = 0
-    while (counter < seconds) {
-        Thread.sleep(1000)
-        counter++
-    }
+    val milliseconds = seconds * 1000L
+    Thread.sleep(milliseconds)
 
     println("Прошло $seconds секунд.")
 }
