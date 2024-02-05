282b66d895b8eaba6dd6050c595268b8a02dcf43
David <ffff0228@bk.ru>
2024-02-05 20:06:15 +0300

    KS-7-3 removed unnecessary variable

--- src/main/kotlin/lesson_7/lesson7_task3.kt
+++ src/main/kotlin/lesson_7/lesson7_task3.kt
@@ -3,7 +3,6 @@ package org.example.lesson_7
 fun main() {
     print("Введите число: ")
     val userNumber = readln().toInt()
-    val evenNumbers = 0..userNumber step 2
 
-    for (i in evenNumbers) println(i)
+    for (i in 0..userNumber step 2) println(i)
 }
