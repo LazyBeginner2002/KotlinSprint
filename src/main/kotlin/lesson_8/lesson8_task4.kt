4f93ea36f57a42bbba2516d2a3dbfe68b5522a66
David <ffff0228@bk.ru>
2024-02-05 22:13:49 +0300

    KS-8-4 removing array element

---
+++ src/main/kotlin/lesson_8/lesson8_task4.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_8
+
+fun main() {
+    val ingredients = arrayOf("Куриная грудка", "Масло", "Помидоры", "Лук", "Зелень", "Майонез", "Соль", "Перец")
+    for (i in ingredients) print("$i ")
+
+    println()
+    println("Какой ингредиент вы хотите заменить?")
+    val oldIngredient = readln()
+
+    if (ingredients.contains(oldIngredient)) {
+        print("Введите название нового ингредиента: ")
+        val newIngredient = readln()
+        ingredients[ingredients.indexOf(oldIngredient)] = newIngredient
+        for (i in ingredients) print("$i ")
+    } else {
+        println("Такого ингредиента в списке нет")
+    }
+}
