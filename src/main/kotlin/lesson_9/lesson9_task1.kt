b0768739f944417120c432e9ec66aac0c97b8bdb
David <ffff0228@bk.ru>
2024-02-05 22:41:20 +0300

    KS-9-1 printing list elements using forEach

---
+++ src/main/kotlin/lesson_9/lesson9_task1.kt
@@ -0,0 +1,7 @@
+package org.example.lesson_9
+
+fun main() {
+    val ingredients = listOf("Куриная грудка", "Масло", "Помидоры", "Лук", "Зелень", "Майонез", "Соль", "Перец")
+
+    ingredients.forEach { println(it) }
+}
