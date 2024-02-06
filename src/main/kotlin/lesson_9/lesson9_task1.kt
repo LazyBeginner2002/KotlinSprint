aa569726a7f92febc5a0a04c0089619a55978b91
David <ffff0228@bk.ru>
2024-02-06 14:15:47 +0300

    KS-9-1 formatted output

--- src/main/kotlin/lesson_9/lesson9_task1.kt
+++ src/main/kotlin/lesson_9/lesson9_task1.kt
@@ -2,5 +2,7 @@ package org.example.lesson_9
 
 fun main() {
     val ingredients = listOf("Куриная грудка", "Масло", "Помидоры", "Лук", "Зелень", "Майонез", "Соль", "Перец")
+
+    println("В списке есть следующие ингредиенты:")
     ingredients.forEach { println(it) }
 }
