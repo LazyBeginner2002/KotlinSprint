b41f5dbea98261f8517a3ac06a654c490c4158ff
David <ffff0228@bk.ru>
2024-02-06 18:58:25 +0300

    KS-9-5 deleted unnecessary file

--- src/main/kotlin/lesson_9/lesson9_task1.kt
+++
@@ -1,7 +0,0 @@
-package org.example.lesson_9
-
-fun main() {
-    val ingredients = listOf("Куриная грудка", "Масло", "Помидоры", "Лук", "Зелень", "Майонез", "Соль", "Перец")
-
-    ingredients.forEach { println(it) }
-}
