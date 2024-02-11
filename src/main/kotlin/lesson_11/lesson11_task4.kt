17315efecafb586f9801d6821b1a4f96a720da6e
David <ffff0228@bk.ru>
2024-02-12 01:34:28 +0300

    KS-11-4 created classes with properties for recipe application

---
+++ src/main/kotlin/lesson_11/lesson11_task4.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_11
+
+fun main() {
+
+}
+
+class Recipe(val recipeDescription: String) {
+
+}
+
+class RecipeCategory(val name: String, val description: String) {
+
+}
+
+class Ingredient(val name: String, val amountOfDishes: Int, val amountOfIngredient: Int, val unit: String) {
+
+}
