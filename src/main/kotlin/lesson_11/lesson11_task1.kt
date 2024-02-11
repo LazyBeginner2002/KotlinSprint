c078eb6b53557c073119972db107e10b0d848417
David <ffff0228@bk.ru>
2024-02-11 17:55:17 +0300

    KS-11-1 created class User and 2 instances

---
+++ src/main/kotlin/lesson_11/lesson11_task1.kt
@@ -0,0 +1,40 @@
+package org.example.lesson_11
+
+fun main() {
+    val user1 = User(
+        1,
+        "user1login",
+        "user1password",
+        "user1@gmail.com",
+    )
+
+    val user2 = User(
+        2,
+        "user2login",
+        "user2password",
+        "user2@gmail.com",
+    )
+
+    println(
+        """
+        User1 properties:
+        id: ${user1.id}
+        login: ${user1.login}
+        password: ${user1.password}
+        e-mail: ${user1.eMail}
+        
+        User2 properties:
+        id: ${user2.id}
+        login: ${user2.login}
+        password: ${user2.password}
+        e-mail: ${user2.eMail}
+    """.trimIndent()
+    )
+}
+
+class User(
+    val id: Int,
+    val login: String,
+    val password: String,
+    val eMail: String,
+)
