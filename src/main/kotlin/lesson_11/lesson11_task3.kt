5d5602f3da80a05677333b24faa6b24df4f7fb2f
David <ffff0228@bk.ru>
2024-02-13 01:17:37 +0300

    KS-11-3 replaced method "updateStatus" and changed listOfMembers data type

--- src/main/kotlin/lesson_11/lesson11_task3.kt
+++ src/main/kotlin/lesson_11/lesson11_task3.kt
@@ -4,15 +4,24 @@ val userStatus = mapOf(1 to "разговаривает", 2 to "микрофон
 
 fun main() {
     val room1 = Room("*Ссылка на drawable resource?*", "Комната 1", mutableListOf())
+    val user1 = User("*Ссылка на drawable resource*", "David", "${userStatus[1]}")
+    user1.showInfo()
+    room1.addMember(user1)
+    room1.updateStatus("David", "${userStatus[2]}")
+    user1.showInfo()
 }
 
 class Room(
     val cover: String,
     val name: String,
-    private val listOfMembers: MutableList<String>,
+    private val listOfMembers: MutableList<User>,
 ) {
     fun addMember(user: User) {
-        listOfMembers.add(user.nickname)
+        listOfMembers.add(user)
+    }
+
+    fun updateStatus(nickname: String, newStatus: String) {
+        listOfMembers.find { it.nickname == nickname }!!.status = newStatus
     }
 }
 
@@ -21,9 +30,6 @@ class User(
     val nickname: String,
     var status: String,
 ) {
-    fun updateStatus(nickname: String, newStatus: String) {
-        status = newStatus
-    }
 
     fun showInfo() {
         println(
