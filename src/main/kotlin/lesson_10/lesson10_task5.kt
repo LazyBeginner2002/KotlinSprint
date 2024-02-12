b46f719a988d3467ae60a956e21dd2efce525094
David <ffff0228@bk.ru>
2024-02-13 00:51:46 +0300

    KS-10-5 fixed some mistakes

--- src/main/kotlin/lesson_10/lesson10_task5.kt
+++ src/main/kotlin/lesson_10/lesson10_task5.kt
@@ -10,21 +10,22 @@ fun main() {
     if (token == null) println("Ошибка при авторизации") else println(showBasket(token))
 }
 
-fun authentication(login: String, password: String): String? {
+fun authenticateAccount(login: String, password: String): String? {
     val lowerCase = 'a'..'z'
     val upperCase = 'A'..'Z'
     val numbers = '0'..'9'
     val characters = lowerCase + upperCase + numbers
 
-    val token = StringBuilder()
-    repeat(32) {
-        token.append(characters.random())
-    }
-    return if (login.count() >= MIN_DATA_LENGTH && password.count() >= MIN_DATA_LENGTH) token.toString()
-    else null
+    if (login.count() >= MIN_DATA_LENGTH && password.count() >= MIN_DATA_LENGTH) {
+        val token = StringBuilder()
+        repeat(32) {
+            token.append(characters.random())
+        }
+        return token.toString()
+    } else return null
 }
 
-fun showBasket (token: String): List<String> {
+fun showBasket(token: String): List<String> {
     val basket = listOf("Товар1", "Товар2", "Товар3", "Товар4")
     return basket
 }
