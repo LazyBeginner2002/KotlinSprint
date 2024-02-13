2d0f241c10a5d9315e861673f08ca2381c299bc0
David <ffff0228@bk.ru>
2024-02-14 02:19:39 +0300

    KS-11-4 added properties to classes

--- src/main/kotlin/lesson_11/lesson11_task4.kt
+++ src/main/kotlin/lesson_11/lesson11_task4.kt
@@ -4,14 +4,15 @@ fun main() {
 
 }
 
-class Recipe(val recipeDescription: String) {
+class Recipe(val image: String, val dishName: String, val recipeDescription: String,
+             val ingredients: List<Ingredient>, cookingSteps: MutableList<String>) {
 
 }
 
-class RecipeCategory(val name: String, val description: String) {
+class RecipeCategory(val image: String, val name: String, val description: String,) {
 
 }
 
-class Ingredient(val name: String, val amountOfDishes: Int, val amountOfIngredient: Int, val unit: String) {
+class Ingredient(val name: String, val amountOfDishes: Int, val amountOfIngredient: Int, val unit: String,) {
 
 }
