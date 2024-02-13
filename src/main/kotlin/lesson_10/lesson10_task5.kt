1d0e9a1f040dcb64fa1e657de83215f2e769751a
David <ffff0228@bk.ru>
2024-02-14 02:21:47 +0300

    KS-10-5 removed else-branch, added a constant

--- src/main/kotlin/lesson_10/lesson10_task5.kt
+++ src/main/kotlin/lesson_10/lesson10_task5.kt
@@ -1,6 +1,7 @@
 package org.example.lesson_10
 
 const val MIN_DATA_LENGTH = 6
+const val NUMBER_OF_CHARACTERS = 32
 
 fun main() {
     val login = "SomeLogin"
@@ -12,18 +13,17 @@ fun main() {
 
 fun authenticateAccount(login: String, password: String): String? {
     if (login.count() < MIN_DATA_LENGTH || password.count() < MIN_DATA_LENGTH) return null
-    else {
-        val lowerCase = 'a'..'z'
-        val upperCase = 'A'..'Z'
-        val numbers = '0'..'9'
-        val characters = lowerCase + upperCase + numbers
 
-        val token = StringBuilder()
-        repeat(32) {
-            token.append(characters.random())
-        }
-        return token.toString()
+    val lowerCase = 'a'..'z'
+    val upperCase = 'A'..'Z'
+    val numbers = '0'..'9'
+    val characters = lowerCase + upperCase + numbers
+
+    val token = StringBuilder()
+    repeat(NUMBER_OF_CHARACTERS) {
+        token.append(characters.random())
     }
+    return token.toString()
 }
 
 fun showBasket(token: String): List<String> {
