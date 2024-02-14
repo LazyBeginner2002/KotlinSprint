64394208ca7c40a42c42b90a2c40eeb8b5327f51
David <ffff0228@bk.ru>
2024-02-14 04:59:26 +0300

    KS-11-4 refactor all classes, removed unnecessary braces

--- src/main/kotlin/lesson_11/lesson11_task4.kt
+++ src/main/kotlin/lesson_11/lesson11_task4.kt
@@ -4,16 +4,26 @@ fun main() {
 
 }
 
-class Recipe(val id: Int, val image: String, val dishName: String, val recipeDescription: String,
-             val ingredients: List<Ingredient>, cookingSteps: MutableList<String>) {
+class Recipe(
+    val id: Int,
+    val image: String,
+    val dishName: String,
+    val recipeDescription: String,
+    val ingredients: List<Ingredient>,
+    val cookingSteps: MutableList<String>
+)
 
-}
+class RecipeCategory(
+    val image: String,
+    val name: String,
+    val description: String
+)
 
-class RecipeCategory(val image: String, val name: String, val description: String,) {
+class Ingredient(
+    val id: Int,
+    val name: String,
+    val amountOfDishes: Int,
+    val amountOfIngredient: Int,
+    val unit: String,
+)
 
-}
-
-class Ingredient(val id: Int, val name: String, val amountOfDishes: Int, val amountOfIngredient: Int,
-                 val unit: String,) {
-
-}
