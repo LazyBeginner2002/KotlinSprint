ed8934b76c198e7777714447f0f095feea2e94b3
David <ffff0228@bk.ru>
2024-02-19 09:53:13 +0300

    KS-13-1 created class with nullable property

---
+++ src/main/kotlin/lesson_13/lesson13_task1.kt
@@ -0,0 +1,7 @@
+package org.example.lesson_13
+
+fun main() {
+    val phoneUser1 = PhoneUser("David", 8926029690, null)
+}
+
+class PhoneUser(name: String, phoneNumber: Long, operatorCompany: String?,)
