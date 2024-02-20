578fa96c4e67a7d5ddc437d01868e6349d42da58
David <ffff0228@bk.ru>
2024-02-20 14:34:19 +0300

    KS-13-3 removed unnecessary parameters from class method

--- src/main/kotlin/lesson_13/lesson13_task2.kt
+++ src/main/kotlin/lesson_13/lesson13_task2.kt
@@ -2,12 +2,12 @@ package org.example.lesson_13
 
 fun main() {
     val phoneUser1 = PhoneUsers("David", 8926029690, "Beeline")
-    phoneUser1.printInfoAboutPhoneUser(phoneUser1.name, phoneUser1.phoneNumber, phoneUser1.operatorCompany)
+    phoneUser1.printInfoAboutPhoneUser()
 }
 
-class PhoneUsers(val name: String, val phoneNumber: Long, val operatorCompany: String? = null) {
-    fun printInfoAboutPhoneUser(name: String, phoneNumber: Long, operatorCompany: String?) {
+class PhoneUsers(private val name: String, private val phoneNumber: Long, private val operatorCompany: String? = null) {
+    fun printInfoAboutPhoneUser() {
         val operator = operatorCompany ?: "<не указано>"
         println("- Имя: $name\n- Номер: $phoneNumber\n- Оператор: $operator")
     }
-} // Не будет ли эффективнее передавать в ф-ию объект?
+}
