ef1aa5cc904b8d3d18b0378cb6b2f0031c41b8f4
David <ffff0228@bk.ru>
2024-02-21 07:50:48 +0300

    KS-12-4 created phone book and method that adds users to it

---
+++ src/main/kotlin/lesson_13/lesson13_task4.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_13
+
+private val phoneBook = mutableListOf<TelephoneUsers>()
+
+fun main() {
+    val user1 = TelephoneUsers("David")
+    print("Введите номер телефона пользователя: ")
+    user1.phoneNumber = readln().toLongOrNull()
+    print("Введите компанию оператора пользователя: ")
+    user1.operatorCompany = readln()
+    addToPhoneBook(user1)
+}
+
+fun addToPhoneBook(user: TelephoneUsers) {
+    if (user.phoneNumber != null) phoneBook.add(user)
+    else println("\nТелефон пользователя не указан")
+}
+
+class TelephoneUsers(val name: String, var phoneNumber: Long? = null, var operatorCompany: String? = null)
